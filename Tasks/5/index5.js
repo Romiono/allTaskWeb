d0f49853bd3dec0116b15017c986c3240b5b76c2
Infekted <mr.freze.roma@mail.ru>
2023-10-08 14:40:55 +0300

    5 task

---
+++ Tasks/5/index5.js
@@ -0,0 +1,15 @@
+const obj = {
+    1: 100,
+    2: 200,
+    3: 300
+}
+
+function onClick(e) {
+    const item = document.getElementById('select')
+    const count = document.getElementById('Count')
+    let result = document.getElementById('result')
+    result.value = obj[item.value] * count.value + '$'
+    e.preventDefault()
+}
+const btn = document.getElementById('button')
+btn.addEventListener('click', onClick)
