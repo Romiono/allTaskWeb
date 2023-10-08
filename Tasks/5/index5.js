9ed2e44b891f5d516f7fa894f43dfb189c3c3b79
Infekted <mr.freze.roma@mail.ru>
2023-10-08 15:10:20 +0300

    5 task(fixPrices)

--- Tasks/5/index5.js
+++ Tasks/5/index5.js
@@ -1,24 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("DOM loaded and parsed");
+    console.log("DOM loaded and parsed")
 });
 
-const obj = {
-    1: 100,
-    2: 200,
-    3: 300
-}
-
 function onClick(e) {
+    const price = [100, 200, 300]
     const item = document.getElementById('select')
     const count = document.getElementById('Count')
     let result = document.getElementById('result')
     if (/^\d+$/.test(count.value)) {
-        result.value = obj[item.value] * count.value + '$'
+        result.value = price[item.value] * count.value + '$'
     }
     else {
         alert('для ввода доступны только числа')
     }
     e.preventDefault()
 }
+
 const btn = document.getElementById('button')
 btn.addEventListener('click', onClick)
