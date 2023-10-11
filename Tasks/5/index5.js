0ca4534e6c72040db7bcc196419f811b88cb84c0
Romiono <roma.serebrennikov.00@mail.ru>
2023-10-11 10:04:45 +0300

    fix

--- Tasks/5/index5.js
+++ Tasks/5/index5.js
@@ -1,20 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("DOM loaded and parsed")
+    console.log("DOM loaded and parsed");
+
+    function onClick(e) {
+        const price = [100, 200, 300];
+        const item = document.getElementById('select');
+        const count = document.getElementById('Count');
+        let result = document.getElementById('result');
+        if (/^\d+$/.test(count.value)) {
+            result.value = price[item.value] * count.value + '$';
+        } else {
+            alert('для ввода доступны только числа');
+        }
+        e.preventDefault();
+    }
+
+    const btn = document.getElementById('button');
+    btn.addEventListener('click', onClick);
 });
 
-function onClick(e) {
-    const price = [100, 200, 300]
-    const item = document.getElementById('select')
-    const count = document.getElementById('Count')
-    let result = document.getElementById('result')
-    if (/^\d+$/.test(count.value)) {
-        result.value = price[item.value] * count.value + '$'
-    }
-    else {
-        alert('для ввода доступны только числа')
-    }
-    e.preventDefault()
-}
 
-const btn = document.getElementById('button')
-btn.addEventListener('click', onClick)
