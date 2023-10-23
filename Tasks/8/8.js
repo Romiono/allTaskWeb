633d1774d7fc1c1942f6c836bf804f4b35b845ef
Infekted <mr.freze.roma@mail.ru>
2023-10-24 00:10:41 +0300

    8 task

--- Tasks/8/8.js
+++ Tasks/8/8.js
@@ -1,26 +1,65 @@
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// // TODO: Add SDKs for Firebase products that you want to use
-//
-// const firebaseConfig = {
-//     apiKey: "AIzaSyAMW-P_tQfiOgNl1AfDdI74ruYdgow0FUQ",
-//     authDomain: "lab-97d8d.firebaseapp.com",
-//     projectId: "lab-97d8d",
-//     storageBucket: "lab-97d8d.appspot.com",
-//     messagingSenderId: "494736102493",
-//     appId: "1:494736102493:web:84cc1d4ccd1df67b7969b4",
-//     measurementId: "G-ZEYEXE3K1X"
-// };
-//
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
 
 const btn = document.getElementById('btn');
 const popUp = document.getElementById('pop-up');
+const form = document.getElementById('form')
+const submit = document.getElementById('submit')
+
+function closePopUp() {
+    popUp.style.display = 'none';
+    btn.style.display = 'block';
+    history.back();
+}
+
+async function post(user) {
+     let response = await fetch('https://formcarry.com/s/7tnYmlnO75', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(user)
+    });
+
+    let result = await response.json();
+    alert(result.message);
+}
 
 btn.addEventListener('click', () => {
-    console.log('click');
-    popUp.style.display = 'block';
+    popUp.style.display = 'flex';
     btn.style.display = 'none';
+    history.pushState({form: 'form'}, 'Title', '?form.html');
 })
+
+popUp.addEventListener('click', (e) => {
+    closePopUp();
+    e.preventDefault();
+})
+
+window.addEventListener('popstate', () => {
+    closePopUp();
+});
+
+form.addEventListener('click', (e) => {
+    e.stopPropagation();
+})
+
+
+
+submit.addEventListener('click', async (e) => {
+    const name = document.getElementById('name')
+    const lastName = document.getElementById('first-name')
+    const mail = document.getElementById('e-mail')
+    const password = document.getElementById('password')
+    const any = document.getElementById('any')
+    e.preventDefault()
+    const user = {
+        name: name.value,
+        lastName: lastName.value,
+        mail: mail.value,
+        password: password.value,
+        any: any.value
+    }
+    console.log(user)
+    await post(user);
+})
+
+
