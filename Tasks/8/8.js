bcd9e3a2562a78ea186146c2cc4bb432a66d8ff9
Romiono <roma.serebrennikov.00@mail.ru>
2023-10-24 09:59:56 +0300

    8 task

--- Tasks/8/8.js
+++ Tasks/8/8.js
@@ -1,66 +1,81 @@
+window.addEventListener("DOMContentLoaded", () => {
+    const btn = document.getElementById('btn');
+    const popUp = document.getElementById('pop-up');
+    const form = document.getElementById('form')
+    const submit = document.getElementById('submit')
 
-const btn = document.getElementById('btn');
-const popUp = document.getElementById('pop-up');
-const form = document.getElementById('form')
-const submit = document.getElementById('submit')
+    function closePopUp(e) {
+        history.back();
+        popUp.style.display = 'none';
+        btn.style.display = 'block';
+    }
 
-function closePopUp() {
-    popUp.style.display = 'none';
-    btn.style.display = 'block';
-    history.back();
-}
+    async function post(user) {
+        try {
+            let response = await fetch('https://formcarry.com/s/7tnYmlnO75', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json;charset=utf-8'
+                },
+                body: JSON.stringify(user)
+            });
 
-async function post(user) {
-     let response = await fetch('https://formcarry.com/s/7tnYmlnO75', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json;charset=utf-8'
-        },
-        body: JSON.stringify(user)
+            let result = await response.json();
+            alert(result.message);
+        } catch (e) {
+        }
+
+    }
+
+    function clear(arr) {
+        arr.foreach((e) => {
+            e[0].value = '';
+        })
+    }
+
+    btn.addEventListener('click', (e) => {
+        popUp.style.display = 'flex';
+        btn.style.display = 'none';
+        history.pushState({form: 'form'}, 'form', '#form.html');
     });
 
-    let result = await response.json();
-    alert(result.message);
-}
+    popUp.addEventListener('click', closePopUp);
 
-btn.addEventListener('click', () => {
-    popUp.style.display = 'flex';
-    btn.style.display = 'none';
-    history.pushState({form: 'form'}, 'Title', '/form.html');
-})
+    form.addEventListener('click', (e) => {
+        e.stopPropagation();
+    })
 
-popUp.addEventListener('click', (e) => {
-    closePopUp();
-    e.preventDefault();
-})
 
-window.addEventListener('popstate', () => {
-    closePopUp();
+    submit.addEventListener('click', async (e) => {
+        const name = document.getElementById('name');
+        const lastName = document.getElementById('first-name');
+        const mail = document.getElementById('e-mail');
+        const password = document.getElementById('password');
+        const any = document.getElementById('any');
+        const check = document.getElementById('check');
+        const form = [[name, 'name'], [lastName, 'lastName'], [mail, 'mail'], [password, 'password'], [any, 'any']];
+        e.preventDefault();
+        closePopUp();
+        // e[0].value = localStorage.getItem(e[1]);
+        // e[0].addEventListener("change",() =>{
+        //     localStorage.setItem(e[1],e[0].value);
+        form.forEach((e) => {
+            e[0].value = localStorage.getItem(e[1])
+            e[0].addEventListener('change', () => {
+                localStorage.setItem(e[1], e[0].value);
+            });
+        });
+        alert(localStorage.getItem('name'));
+        const user = {
+            name: name.value,
+            lastName: lastName.value,
+            mail: mail.value,
+            password: password.value,
+            any: any.value
+        }
+        console.log(user);
+        clear(form);
+        await post(user);
+
+    });
 });
-
-form.addEventListener('click', (e) => {
-    e.stopPropagation();
-})
-
-
-
-submit.addEventListener('click', async (e) => {
-    const name = document.getElementById('name')
-    const lastName = document.getElementById('first-name')
-    const mail = document.getElementById('e-mail')
-    const password = document.getElementById('password')
-    const any = document.getElementById('any')
-    e.preventDefault()
-    closePopUp()
-    const user = {
-        name: name.value,
-        lastName: lastName.value,
-        mail: mail.value,
-        password: password.value,
-        any: any.value
-    }
-    console.log(user)
-    await post(user);
-})
-
-
